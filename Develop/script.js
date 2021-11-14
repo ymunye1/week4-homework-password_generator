2330d27984cabfdc09d3d0238bfcc02ffde664af
Yasin Munye <cyasinsadiq@gmail.com>
2021-11-13 23:59:23 -0500

    added more variable and strings

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,26 @@
 //Assignment Code
+
+
+//created a global variable named messagePrompt
+var messagePrompt;
+//created a global variable named lowerCaseConfirm
+var lowerCaseConfirm;
+//created a global variable named upperCaseConfirm
+var upperCaseConfirm;
+//created a global variable named numbersConfirm
+var numbersConfirm;
+//created a global variable named specialCharConfirm
+var specialCharConfirm;
+//created a global variable named randomLowerCaseAns
+var randomLowerCaseAns;
+//created a global variable named randomUpperCaseAns
+var randomUpperCaseAns;
+//created a global variable named randomNumbersAns
+var randomNumbersAns;
+//created a global variable named randomSpecialCharAns
+var randomSpecialCharAns;
+
+
 //pull the button from html id and store it to a varible generateButton
 var generateButton = document.querySelector("#generate");
 
@@ -7,20 +29,25 @@ function writePassword() {
   var password = generatePassword();
   //placeholder for generated password. pulled from id password on html page and stored in variable passwordText
   var passwordText = document.querySelector("#password");
-  var messagePrompt;
+  
 
   passwordText.value = password;
 
 }
 
-// array for numbers
-var numbers = [0,1,2,3,4,5,6,7,8,9]
+
 // array for lower case letters
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 //array for uppercase letters, calling lowercase letters then mapping to make upper case letters
 var upperCaseLetters= (lowerCaseLetters.map(lowerCaseLetters => lowerCaseLetters .toUpperCase()));
+// array for numbers
+var numbers = [0,1,2,3,4,5,6,7,8,9];
 //arrays for special characters
 var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "{", "[", "}", "]", "|", ";", ":", "'", '"',  ',',  "<", ">", '.', '?', '/'];
+
+var array1=[randomLowerCaseAns, randomUpperCaseAns, randomNumbersAns,randomSpecialCharAns]
+
+
 
 //create a function named questionOne 
 function questionOne() {
@@ -62,15 +89,42 @@ function questionOne() {
     }
   }
   
-  //created a global variable named lowerCaseConfirm
-  var lowerCaseConfirm;
-  //created a global variable named upperCaseConfirm
-  var upperCaseConfirm;
-  //created a global variable named numbersConfirm
-  var numbersConfirm;
-  //created a global variable named specialCharConfirm
-  var specialCharConfirm;
   
+  // create a function named randomLowerCase and select a random lowercase letter in the array and generate it
+    function randomLowerCase (lowerCaseLetters) {
+    return lowerCaseLetters[Math.floor(Math.random()*lowerCaseLetters.length)];
+  }
+  // take the generated character and store it in an variable named randomLowerCaseAns
+    randomLowerCaseAns= randomLowerCase(lowerCaseLetters)
+
+
+  // create a function named randomUpperCase and select a random UpperCase letter in the array and generate it
+    function randomUpperCase (upperCaseLetters) {
+    return upperCaseLetters[Math.floor(Math.random()*upperCaseLetters.length)];
+  }
+  // take the generated character and store it in an variable named randomUpperCaseAns 
+    randomUpperCaseAns = randomUpperCase(upperCaseLetters)
+
+  
+    // create a function named randomNumbers and select a random number in the array and generate it
+  function randomNumbers (numbers) {
+    return numbers[Math.floor(Math.random()*numbers.length)];
+  }
+  // take the generated character and store it in an variable named randomNumbersAns
+    randomNumbersAns = randomNumbers(numbers)
+
+  
+    // create a function named randomSpecialChar and select a random specialCharacters in the array and generate it
+  function randomSpecialChar (specialCharacters) {
+    return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+  }
+  // take the generated character and store it in an variable named randomSpecialCharAns
+    randomSpecialCharAns = randomSpecialChar(specialCharacters)
+  
+  
+  
+
+
   
   //create a function named questionTwo and passes message lowercase as true or false.
   function questionTwo(messageLowerCase){
@@ -136,4 +190,37 @@ generateButton.addEventListener("click", questionOne)
 
 // // WHEN the password is generated
 // // THEN the password is either displayed in an alert or written to the page
+//arrays for random genarated charcters
+var convertedStringToNumber = Number(messagePrompt)
 
+
+var generatedArray = new Array();
+ if (messagePrompt <129 && messagePrompt >7)
+
+ var count = 0;
+for(var k=0; k <messagePrompt; k++){
+    console.log("WE AHVE AN ANSWER")
+}
+
+//}
+if(lowerCaseConfirm){
+  generatedArray[count] = randomLowerCaseAns;
+  count++;
+}
+
+if(upperCaseConfirm){
+  generatedArray[count] = randomUpperCaseAns;
+  count++;
+}
+if(numbersConfirm){
+  generatedArray[count] = randomNumbersAns;
+  count++;
+}
+if(specialCharConfirm) {
+  generatedArray[count] =randomSpecialCharAns;
+  count++;
+}
+
+
+
+// console.log(generatedArray);
